123f99801e458ed50943abc9de0bc8764fdc0955
Mingli Yuan <mingli.yuan@gmail.com>
2013-07-19 23:44:02 +0800

    change provisioning process

---
+++ scripts/upgrade.rb
@@ -0,0 +1,19 @@
+#! /usr/bin/env ruby
+
+require 'yaml'
+
+provision = YAML.load_file(ARGV[0])
+provision.each_key do |lname|
+  layer = provision[lname]
+  layer.each do |entry|
+    entry.each_key do |name|
+      version = entry[name]
+      invoker = "#{ENV['BTF_HOME']}/scripts/#{name}/upgrade"
+      IO.popen [invoker, version] if File.file?(invoker)
+    end
+  end
+end
+
+
+
+
